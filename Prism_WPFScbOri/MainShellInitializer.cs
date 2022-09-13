49a26cd5d1c121e126b11ceac41d8b35cba7a474
MonoDever <60083599+MonoDever@users.noreply.github.com>
2022-09-13 13:27:47 +0700

    Add files via upload

---
+++ Prism_WPFScbOri/MainShellInitializer.cs
@@ -0,0 +1,25 @@
+﻿using LoginModule;
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prism_WPFScbOri
+{
+    public class MainShellInitializer : IMainShellInitializer
+    {
+        public void ConfigModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<LoginModule.LoginModules>();
+            moduleCatalog.AddModule<MainModule.MainModules>();
+        }
+
+        public void RegisterType(IContainerRegistry containnerRegister)
+        {
+            
+        }
+    }
+}
